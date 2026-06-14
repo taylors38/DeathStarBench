d7686ec87c522a21b747f95d1afd1bf25d3b727b
Taylor <tss0201@mavs.uta.edu>
2026-06-14 02:16:56 +0000

    hotelReservation running kubernetes config

--- hotelReservation/kubernetes/scripts/build-docker-images.sh
+++ hotelReservation/kubernetes/scripts/build-docker-images.sh
@@ -14,12 +14,12 @@ cd ../
 ROOT_FOLDER=$(pwd)
 $EXEC create --name mybuilder --use
 
-for i in hotelreservation #frontend geo profile rate recommendation reserve search user #uncomment to build multiple images
+for i in hotelreservation # frontend geo profile rate recommendation reserve search user #uncomment to build multiple images
 do
   IMAGE=${i}
   echo Processing image ${IMAGE}
   cd $ROOT_FOLDER
-  $EXEC build -t "$USER"/"$IMAGE":"$TAG" -f Dockerfile . --platform linux/arm64,linux/amd64 --push
+  $EXEC build -t "$USER"/"$IMAGE":"$TAG" -f Dockerfile . --platform linux/arm64,linux/amd64 --load
   cd $ROOT_FOLDER
 
   echo
--- hotelReservation/wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua
+++ hotelReservation/wrk2/scripts/hotel-reservation/mixed-workload_type_1.lua
@@ -2,7 +2,7 @@ local socket = require("socket")
 math.randomseed(socket.gettime()*1000)
 math.random(); math.random(); math.random()
 
-local url = "http://localhost:5000"
+local url = "http://localhost:30712"
 
 local function get_user()
   local id = math.random(0, 500)
